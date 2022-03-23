ad2e8c383d4e66fee800aacc3f8d0f2d45a2aa0a
takagi <iam.takagi.dev@gmail.com>
2022-03-23 11:34:30 +0900

    [web/server] Fix: serve public path

--- web/src/server/index.ts
+++ web/src/server/index.ts
@@ -3,10 +3,10 @@ import path from "path"
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "..",  "..", "public")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.get('*', (req, res, next) => {
-    res.sendFile(path.join(__dirname, "..", "..", "public", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 })
 
 app.listen(process.env.PORT || 8080);
