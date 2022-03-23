2dd1438112e55bedd414071b8ff72d5bae02d19d
takagi <iam.takagi.dev@gmail.com>
2022-03-23 11:48:53 +0900

    [backend/gunicorn.py] Fix: port

--- backend/gunicorn.py
+++ backend/gunicorn.py
@@ -1,4 +1,4 @@
-bind = '0.0.0.0:3000'
+bind = '0.0.0.0:8500'
 accesslog = './log/gunicorn/access.log'
 access_log_format = '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 errorlog = './log/gunicorn/error.log'
