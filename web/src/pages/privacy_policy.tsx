3195629dbe2ac4982c945ec7f09b3670bf9acad5
takagi <iam.takagi.dev@gmail.com>
2022-03-27 09:15:06 +0900

    Update privacy_policy.tsx

--- web/src/pages/privacy_policy.tsx
+++ web/src/pages/privacy_policy.tsx
@@ -21,7 +21,6 @@ const PrivacyPolicy = () => (
           このGoogleアナリティクスはトラフィックデータの収集のためにCookieを使用しています。このトラフィックデータは匿名で収集されており、個人を特定するものではありません。この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。この規約に関して、詳しくは
           <a
             href="https://policies.google.com/privacy"
-            className="border-b hover:border-purple-800 text-purple-800 border-transparent"
           >
             こちら
           </a>
