52477c09548a58cfdf1c18720490359d4df65720
takagi <iam.takagi.dev@gmail.com>
2022-05-02 01:13:04 +0900

    Fix: api url

--- web/src/consts.ts
+++ web/src/consts.ts
@@ -1,7 +1,7 @@
 const isProduction = process.env.NODE_ENV === "production"
 
 export const APP_NAME = "TwiChat" as const
-export const API_BASE_URL = isProduction ? "https://twichat.app/api/" : "http://localhost:8500/api/" as const
+export const API_BASE_URL = isProduction ? "https://twichat.iamtakagi.net/api/" : "http://localhost:8500/api/" as const
 export const OPPONENT_MESSAGE_DELAY = 500
 export const TWITTER_ID_REGEX = new RegExp("/[0-9a-zA-Z_]{1,15}")
 export const GOOGLE_ANALYTICS_ID = "G-E7C1H3PT4H"
